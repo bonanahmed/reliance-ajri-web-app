001ea43d4e510ed3a6492be770bd6835f2361f78
Aditya <adityapratamax11@gmail.com>
2022-10-24 13:41:59 +0700

    update

--- database/seeders/VariabelSeeder.php
+++ database/seeders/VariabelSeeder.php
@@ -86,6 +86,20 @@ class VariabelSeeder extends Seeder
                 'value' => 'Tetap Update disetiap aktivitas kita',
                 'content' => null,
                 'image' => null
+            ],
+            [
+                'var' => 'home_section_1',
+                'value' => 'Cukup Bayar Sesuai Resiko',
+                'content' => '<div>PT Asuransi Jiwa Reliance Indonesia (AJRI) merupakan perusahaan yang berada dalam payung dari group PT Reliance Capital Management dengan visi untuk menjadi sebuah institusi keuangan kelas dunia yang menyediakan solusi keuangan dengan komprehensif bagi seluruh nasabahnya</div>',
+                'image' => null,
+                'created_by' => 1,
+            ],
+            [
+                'var' => 'home_section_1_button',
+                'value' => 'Lihat Selengkapnya',
+                'content' => null,
+                'image' => null,
+                'created_by' => 1,
             ]
         ]);
     }
