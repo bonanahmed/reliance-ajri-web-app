ab2010d1d34734c72baf4513c18a4d882ce8786f
Aditya <adityapratamax11@gmail.com>
2022-11-12 10:40:12 +0700

    add seeder mitra

--- database/seeders/MitraSeeder.php
+++ database/seeders/MitraSeeder.php
@@ -14,6 +14,108 @@ class MitraSeeder extends Seeder
      */
     public function run()
     {
-        Mitra::factory(30)->create();
+        // Mitra::factory(30)->create();
+        Mitra::insert([
+            [
+                'name' => 'CV Handayani',
+                'type' => 'rekanan',
+                'description' => '<div>Animi quia dolore quia. Dolorum sequi alias quis veniam et ut sunt similique. Velit voluptate ipsum vero sed. Quaerat autem dolore sunt.</div>',
+                'image' => 'mitra-image/2GZFPSqBnEkdWvO6y6hQ0d0jsomFxMNmG1OYJNOM.png',
+                'tanggal_kerjasama' => NULL,
+                'created_by' => NULL,
+                'created_at' => '2022-10-31 12:39:37',
+                'updated_at' => '2022-11-08 16:00:02'
+            ],
+            [
+                'name' => 'CV Nugroho',
+                'type' => 'rekanan',
+                'description' => '<div>Repellendus possimus est sequi porro pariatur. Dolorem tempore aperiam voluptatibus qui in neque quo. Sunt ipsam quia eos occaecati. Voluptas esse praesentium sapiente rerum accusantium eos nam.</div>',
+                'image' => 'mitra-image/MH68Vk3EmKaGfxucKMTuIjebloTieMw9YTnwxfBQ.png',
+                'tanggal_kerjasama' => NULL,
+                'created_by' => NULL,
+                'created_at' => '2022-10-31 12:39:37',
+                'updated_at' => '2022-11-08 16:02:07'
+            ],
+            [
+                'name' => 'UD Anggriawan Widiastuti Tbk',
+                'type' => 'client',
+                'description' => 'Amet voluptatibus doloribus magni laboriosam aut. Sit ullam consequatur sed hic esse laborum in. Aut laborum libero iusto qui dolores aut explicabo. Qui voluptatem quibusdam beatae laborum vero excepturi autem.',
+                'image' => NULL,
+                'tanggal_kerjasama' => NULL,
+                'created_by' => NULL,
+                'created_at' => '2022-10-31 12:39:37',
+                'updated_at' => '2022-10-31 12:39:37'
+            ],
+            [
+                'name' => 'CV Kusmawati (Persero) Tbk',
+                'type' => 'rekanan',
+                'description' => '<div>Rem animi et sunt minus. Sint quo nulla quia cum magni in culpa. Commodi aut qui minima voluptatibus unde expedita.</div>',
+                'image' => 'mitra-image/rHoYWGPA38HVdu93BN5exePv6OnETJo6NAuqihva.png',
+                'tanggal_kerjasama' => NULL,
+                'created_by' => NULL,
+                'created_at' => '2022-10-31 12:39:37',
+                'updated_at' => '2022-11-08 16:00:19'
+            ],
+            [
+                'name' => 'CV Farida',
+                'type' => 'rekanan',
+                'description' => '<div>Maxime et ut voluptatem rem quis. Non delectus sit cupiditate ab et minima ex quia. Blanditiis sit rerum fuga qui unde eveniet.</div>',
+                'image' => 'mitra-image/Fiyu73UgzLHW0UtpYEe2DE2Mco4NgeSG7Zcg2XQf.png',
+                'tanggal_kerjasama' => NULL,
+                'created_by' => NULL,
+                'created_at' => '2022-10-31 12:39:37',
+                'updated_at' => '2022-11-08 16:01:57'
+            ],
+            [
+                'name' => 'CV Sihotang Uwais (Persero) Tbk',
+                'type' => 'rekanan',
+                'description' => '<div>Et corrupti praesentium sint. Voluptatem in vel nisi aliquam nihil asperiores corporis tempora.</div>',
+                'image' => 'mitra-image/nFL3ZcKwK6IAoKF1c2WTHbEjJPsnnUGso6WBY6kv.png',
+                'tanggal_kerjasama' => NULL,
+                'created_by' => NULL,
+                'created_at' => '2022-10-31 12:39:37',
+                'updated_at' => '2022-11-08 16:00:41'
+            ],
+            [
+                'name' => 'PT Purwanti Uyainah',
+                'type' => 'client',
+                'description' => 'Est nostrum qui veniam veritatis magni. Exercitationem tempora in voluptate alias. Eos molestias minus cumque quia ut voluptatum blanditiis. Quasi et et odit omnis rem dolor quia.',
+                'image' => NULL,
+                'tanggal_kerjasama' => NULL,
+                'created_by' => NULL,
+                'created_at' => '2022-10-31 12:39:37',
+                'updated_at' => '2022-10-31 12:39:37'
+            ],
+            [
+                'name' => 'CV Hasanah Tbk',
+                'type' => 'rekanan',
+                'description' => '<div>Et quia voluptatem ut est. Itaque ab error corrupti consectetur. Enim neque illo minima tempore illum. A optio ipsum minus provident quis est vero.</div>',
+                'image' => 'mitra-image/oIMAZFCNVNs1SNlm74cZV6EkUGG7sPlc6jFmHnef.png',
+                'tanggal_kerjasama' => NULL,
+                'created_by' => NULL,
+                'created_at' => '2022-10-31 12:39:37',
+                'updated_at' => '2022-11-08 16:00:10'
+            ],
+            [
+                'name' => 'PT Prabowo Wibisono Tbk',
+                'type' => 'rekanan',
+                'description' => 'Quis architecto explicabo deserunt expedita. Architecto omnis ipsum porro sed ex. Unde quisquam aliquam alias voluptatem repudiandae sed. Optio rerum porro qui voluptatem officia impedit et. Quae consequuntur libero voluptas eius magni odio quod.',
+                'image' => NULL,
+                'tanggal_kerjasama' => NULL,
+                'created_by' => NULL,
+                'created_at' => '2022-10-31 12:39:37',
+                'updated_at' => '2022-10-31 12:39:37'
+            ],
+            [
+                'name' => 'PT Sihombing Wijayanti',
+                'type' => 'client',
+                'description' => 'Qui qui omnis quidem quia dolor unde ut. Necessitatibus natus sequi velit sunt eveniet voluptas. Nesciunt corrupti in soluta. Et dicta et eum debitis officia esse.',
+                'image' => NULL,
+                'tanggal_kerjasama' => NULL,
+                'created_by' => NULL,
+                'created_at' => '2022-10-31 12:39:37',
+                'updated_at' => '2022-10-31 12:39:37'
+            ]
+        ]);
     }
 }
