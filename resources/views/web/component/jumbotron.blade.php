d35fed7330ef382cf9d408c1384be09c13c9e7bf
Aditya <adityapratamax11@gmail.com>
2023-01-02 13:44:40 +0700

    add variabel home_title for jumbotron beranda

--- resources/views/web/component/jumbotron.blade.php
+++ resources/views/web/component/jumbotron.blade.php
@@ -5,10 +5,10 @@
                 <div class="pt-5 mt-5">
                     <p style="margin: 0px;color:#29559B; font-size:20px">Reliance</p>
                     <p style="color:#29559B;font-size:53px;font-weight:700; font-style:normal;letter-spacing: 0.025em;">
-                        {{ Request::get('variabel')->produk_title->value ?? 'produk_title' }}
+                        {{ Request::get('variabel')->home_title->value ?? 'home_title' }}
                     </p>
                     <p style="color:#737373;font-style: normal;font-weight: 400;font-size: 31px;">{{
-                        Request::get('variabel')->produk_sub_title->value ?? 'produk_sub_title' }}</p>
+                        Request::get('variabel')->home_sub_title->value ?? 'home_sub_title' }}</p>
                     <a href="{{
                         Request::get('variabel')->whatsapp_url->value ?? '#' }}" target="_blank" type=" button" class="btn btn-md" style="background-color: #29559B;color:aliceblue;padding:16px 32px; border-radius:12px">
                         <i class="fa-brands fa-whatsapp"></i> Konsultasi Sekarang
