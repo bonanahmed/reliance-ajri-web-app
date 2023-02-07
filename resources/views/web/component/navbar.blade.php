d464d47bf091f24566b84b3527a19215f440b6ee
Aditya <adityapratamax11@gmail.com>
2023-02-07 14:07:51 +0700

    add link laporan keuangan at navbar FE

--- resources/views/web/component/navbar.blade.php
+++ resources/views/web/component/navbar.blade.php
@@ -5,23 +5,19 @@
                 @if(!isset(Request::get('variabel')->navbar_logo->image))
                 <img src="{{ asset('assets/img/logo-1.png') }}" alt="Reliance Logo" title="Reliance Logo" height="30">
                 @else
-                <img src="{{ asset('storage/'.Request::get('variabel')->navbar_logo->image) }}" alt="Reliance Logo"
-                    title="Reliance Logo" height="30">
+                <img src="{{ asset('storage/'.Request::get('variabel')->navbar_logo->image) }}" alt="Reliance Logo" title="Reliance Logo" height="30">
                 @endif
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample"
-                aria-controls="offcanvasExample">
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mx-auto mb-2 mb-lg-0 navbar-custom">
                     <li class="nav-item mx-1">
-                        <a class="nav-link {{ Request::is('/') ? 'active' : '' }}" aria-current="page"
-                            href="/">Beranda</a>
+                        <a class="nav-link {{ Request::is('/') ? 'active' : '' }}" aria-current="page" href="/">Beranda</a>
                     </li>
                     <li class="nav-item dropdown mx-3">
-                        <a class="nav-link {{ Request::is('about/*') ? 'active' : '' }} dropdown-toggle" href="#"
-                            id="dropdown_about_us" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link {{ Request::is('about/*') ? 'active' : '' }} dropdown-toggle" href="#" id="dropdown_about_us" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Tentang Kami
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="dropdown_about_us">
@@ -29,11 +25,11 @@
                             <li><a class="dropdown-item" href="/about/{{ $item_about->slug }}">{{ $item_about->title
                                     }}</a></li>
                             @endforeach
+                            <li><a class="dropdown-item" href="/keuangan">Laporan Keuangan</a></li>
                         </ul>
                     </li>
                     <li class="nav-item dropdown mx-3">
-                        <a class="nav-link {{ Request::is('produk/*') ? 'active' : '' }} dropdown-toggle" href="#"
-                            id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link {{ Request::is('produk/*') ? 'active' : '' }} dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Produk
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
@@ -55,16 +51,15 @@
                         <a class="nav-link {{ Request::is('mitra/*') ? 'active' : '' }} dropdown-toggle" id="navbarMitraScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" href="#">Mitra</a>
                         <ul class="dropdown-menu" aria-labelledby="navbarMitraScrollingDropdown">
                             <li><a class="dropdown-item" href="/mitra/klien">Klien</a></li>
-                            <li><a class="dropdown-item" href="/mitra/rekanan">Rekanan</a></li>         
+                            <li><a class="dropdown-item" href="/mitra/rekanan">Rekanan</a></li>
                         </ul>
                     </li>
                     <li class="nav-item dropdown mx-3">
-                        <a class="nav-link {{ Request::is('klaim/*') ? 'active' : '' }} dropdown-toggle"
-                        id="navbarKlaimScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" href="#">Klaim</a>
+                        <a class="nav-link {{ Request::is('klaim/*') ? 'active' : '' }} dropdown-toggle" id="navbarKlaimScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" href="#">Klaim</a>
                         <ul class="dropdown-menu" aria-labelledby="navbarKlaimScrollingDropdown">
                             <li><a class="dropdown-item" href="/klaim/prosedur">Prosedur Klaim</a></li>
-                            <li><a class="dropdown-item" href="/klaim/faq">FAQ Klaim</a></li>         
-                            <li><a class="dropdown-item" href="/klaim/info">Info Klaim</a></li>         
+                            <li><a class="dropdown-item" href="/klaim/faq">FAQ Klaim</a></li>
+                            <li><a class="dropdown-item" href="/klaim/info">Info Klaim</a></li>
                         </ul>
                     </li>
                     <li class="nav-item mx-3">
@@ -91,11 +86,9 @@
 </div>
 
 
-<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel"
-    style="max-width:300px">
+<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel" style="max-width:300px">
     <div class="offcanvas-header mx-3" style="">
-        <img src="{{ asset('storage/'.Request::get('variabel')->navbar_logo->image) }}" alt="Reliance Logo"
-            title="Reliance Logo" height="30">
+        <img src="{{ asset('storage/'.Request::get('variabel')->navbar_logo->image) }}" alt="Reliance Logo" title="Reliance Logo" height="30">
         <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body">
@@ -104,12 +97,10 @@
                 <a class="nav-link {{ Request::is('/') ? 'active' : '' }}" aria-current="page" href="/">Beranda</a>
             </li>
             <li class="nav-item mx-3">
-                <a class="nav-link {{ Request::is('about/*') ? 'active' : '' }}"
-                    href="/about/{{ $about_list->first()->slug ?? '' }}">Tentang Kami</a>
+                <a class="nav-link {{ Request::is('about/*') ? 'active' : '' }}" href="/about/{{ $about_list->first()->slug ?? '' }}">Tentang Kami</a>
             </li>
             <li class="nav-item mx-3">
-                <a class="nav-link {{ Request::is('produk/*') ? 'active' : '' }}"
-                    href="/produk/kumpulan/{{ $produk_kumpulan->first()->slug ?? '' }}">Produk</a>
+                <a class="nav-link {{ Request::is('produk/*') ? 'active' : '' }}" href="/produk/kumpulan/{{ $produk_kumpulan->first()->slug ?? '' }}">Produk</a>
             </li>
             <li class="nav-item mx-3">
                 <a class="nav-link {{ Request::is('mitra/*') ? 'active' : '' }}" href="/mitra/klien">Mitra</a>
