2ed595aa715f41f57b2846b9a7f73fe315ac2182
aili <ahmadsuhaili244@gmail.com>
2023-02-10 21:55:59 +0700

    add search google console

--- resources/views/web/component/main.blade.php
+++ resources/views/web/component/main.blade.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="@yield('description')">
     <meta name="keyword" content="@yield('keywords')">
+    <meta name="google-site-verification" content="qChy-Mdd8Pbh3uZe1TMTz2e2twjjFIkUp5GS4eSnctE" />
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
